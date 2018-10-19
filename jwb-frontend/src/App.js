0efeb7506ba13c630644bc8ba98b47f7492c3ad3
Owen Auch <owenauch@wustl.edu>
2018-10-19 00:33:47 -0500

    react-create-app on Heroku

--- jwb-frontend/src/App.js
+++ jwb-frontend/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './overflow-01.png';
 import './App.css';
+import axios from "axios"
 
 import Input from '@material-ui/core/Input';
 import Typography from '@material-ui/core/Typography';
@@ -10,10 +11,28 @@ import Button from '@material-ui/core/Button'
 class App extends Component {
   state = {
     name: '',
+    targetInfo: null
   };
 
   handleChange = event => {
     this.setState({ name: event.target.value });
+  };
+
+  handleButtonClick = event => {
+    const url = "https://still-oasis-87334.herokuapp.com/" + this.state.name
+    axios.get(url, {crossdomain: true})
+    .then((response) => {
+      console.log(response)
+      this.setState({ targetInfo: response.data })
+    })
+    .catch((error) => {
+      console.log(error)
+      this.setState({ targetInfo: {
+        target: "Error: our website is busy teaching robots how to love.",
+        code: "N/A",
+        target_pic: "https://media.giphy.com/media/3kFkjoddRSOMXVxAHI/giphy.gif"
+      }})
+    })
   };
 
   render() {
@@ -37,6 +56,19 @@ class App extends Component {
             <Button onClick={this.handleButtonClick} style={{backgroundColor: "black", color: "white", fontSize: "15pt"}}>
               Show me who to kill
             </Button>
+            {this.state.targetInfo && <Paper className="targetInfoCard" style={{backgroundColor: "black", color: "white", padding: "50px"}}>
+              <Typography component="h1" variant="h4" style={{color: "white"}}>
+                {this.state.targetInfo.target}
+              </Typography>
+              <Typography component="h1" variant="h5" style={{color: "white", margin: "10px"}}>
+                Codename: {this.state.targetInfo.code}
+              </Typography>
+                <img
+                  alt="Target Image"
+                  src={this.state.targetInfo.target_pic}
+                  style={{maxHeight: "500px"}}
+                />
+            </Paper>}
           </div>
         </Paper>
       </div>
